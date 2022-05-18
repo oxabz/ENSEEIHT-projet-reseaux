3070d8323881f1db4b62581022450870eb957a84
greenananas <greenananas38@gmail.com>
2022-05-18 17:21:58 +0200

    fix iptables

--- config/entreprise-interne/vm-routeur/config/iptables.py
+++ config/entreprise-interne/vm-routeur/config/iptables.py
@@ -2,8 +2,9 @@
 
 from subprocess import run
 
+
 def start():
-    external_int_name = "enp0s9"
+    external_int_name = "enp0s8"
     external_int_ip = "120.0.24.14"
 
     run("iptables -t nat -A POSTROUTING -o {int} -j SNAT --to-source {ip}".format(int=external_int_name, ip=external_int_ip).split(" "))
