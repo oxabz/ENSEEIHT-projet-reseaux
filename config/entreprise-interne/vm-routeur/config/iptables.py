827fb4c6812f431fb86f57cacf29e9282b03ed50
greenananas <greenananas38@gmail.com>
2022-05-23 08:37:17 +0200

    update wireguard script and iptables

--- config/entreprise-interne/vm-routeur/config/iptables.py
+++ config/entreprise-interne/vm-routeur/config/iptables.py
@@ -15,16 +15,27 @@ def start():
     cli_server_ip = "10.0.0.40"
     cli_server_ext_ssh_port = "223"
 
+    # Clear all iptables entries
+    run("iptables -F".split(" "))
+    run("iptables -t nat -F".split(" "))
+    run("iptables -t mangle -F".split(" "))
+
+    # Add Source NAT rule to enable internal clients to communicate with the outside
     run("iptables -t nat -A POSTROUTING -o {int} -j SNAT --to-source {ip}".format(int=external_int_name, ip=external_int_ip).split(" "))
 
+    # Add Destination NAT rule to enable external clients to connect with internal clients via SSH
+    # This needs to be disabled after the configuration for security reasons
+    # NAT rule for the DNS server
     run("iptables -t nat -A PREROUTING -p tcp -d {dip} --dport {dport} -j DNAT --to-destination {rip}:22"
         .format(dip=external_int_ip, dport=dns_server_ext_ssh_port, rip=dns_server_ip)
         .split(" "))
 
+    # NAT rule for the application server
     run("iptables -t nat -A PREROUTING -p tcp -d {dip} --dport {dport} -j DNAT --to-destination {rip}:22"
         .format(dip=external_int_ip, dport=app_server_ext_ssh_port, rip=app_server_ip)
         .split(" "))
 
+    # NAT rule for the client computer
     run("iptables -t nat -A PREROUTING -p tcp -d {dip} --dport {dport} -j DNAT --to-destination {rip}:22"
         .format(dip=external_int_ip, dport=cli_server_ext_ssh_port, rip=cli_server_ip)
         .split(" "))
--- config/entreprise-interne/vm-routeur/wireguard/wireguard.py
+++ config/entreprise-interne/vm-routeur/wireguard/wireguard.py
@@ -4,28 +4,16 @@ from subprocess import run
 
 
 def start():
-    int_ip = "10.10.10.1/24"
-    listen_port = "51820"
+    # Create the wireguard configuration files directory
+    run("mkdir -p /etc/wireguard/".split(" "))
 
-    # create the wireguard configuration files directory
-    run("mkdir -p /etc/wireguard".split(" "))
+    # Copy wireguard keys to wireguard configuration directory
+    run("cp ./wireguard/publickey /etc/wireguard/".split(" "))
+    run("cp ./wireguard/privatekey /etc/wireguard/".split(" "))
 
-    # generate private and public key pair
-    run("wg genkey | sudo tee privatekey | wg pubkey | sudo tee /etc/wireguard/publickey".split(" "))
+    # Copy wireguard configuration file to wireguard configuration directory
+    run("cp ./wireguard/wg0.conf".split(" "))
 
-    privatekey = run("cat privatekey".split(" "))
-
-    # wireguard config file
-    wg_config = """
-    [Interface]
-    PrivateKey = {pkey}
-    Address = {ip}
-    ListenPort = {port}
-    SaveConfig = true
-    """.format(pkey=privatekey, ip=int_ip, port=listen_port)
-
-    run(["echo", wg_config])
-
-    # run("echo '{}' > /etc/wireguard/wg0.conf".format(wg_config))
-
-    # run("".split(" "))
+    # Start and enable wireguard service
+    run("systemctl enable wg-quick@wg0.service".split(" "))
+    run("systemctl start wg-quick@wg0.service".split(" "))
